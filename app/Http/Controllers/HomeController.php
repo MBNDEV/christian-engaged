9b8e3dc31746607b2ec45155ec65114e7ce9ea82
iamkenken <kenneth.froyalde23@gmail.com>
2021-08-12 16:20:43 +0800

    test type

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -188,7 +188,7 @@ class HomeController extends Controller {
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
         $products = curl_exec($ch);
         curl_close($ch); 
-        echo(gettype($newvideo));
+        echo(gettype(json_decode($products, true)));
         $data['content'] = view('web.homepage_demo', compact('donationGoal', 'videos', 'goalPercent', 'resultsocial', 'videoIframe', 'aboutUsPageSlug', 'videoPageSlug', 'merchPageSlug', 'newvideo', 'products'));   
         // return view('layouts.homepage-template', $data);
     }
