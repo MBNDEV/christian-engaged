52a4324a7dcad8c7146c8bd8f6b83b8e8a2b4a12
iamkenken <kenneth.froyalde23@gmail.com>
2021-08-20 17:29:06 +0800

    update sched

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -52,7 +52,7 @@ class Kernel extends ConsoleKernel {
         })->monthlyOn(10, '02:31');
 
         $schedule->command('ce:recurring-donation')
-        ->dailyAt('16:55');
+        ->dailyAt('09:30');
     }
 
     /**
