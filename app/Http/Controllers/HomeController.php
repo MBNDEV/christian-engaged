5cd1442f9b92824f88d7fbf314c522f4ac539311
iamkenken <kenneth.froyalde23@gmail.com>
2021-09-30 17:56:05 +0800

    remove var dump

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -189,9 +189,8 @@ class HomeController extends Controller {
         $r = curl_exec($ch);
         curl_close($ch); 
         $products = json_decode($r, true);
-        var_dump($products);
-        // $data['content'] = view('web.homepage_demo', compact('donationGoal', 'videos', 'goalPercent', 'resultsocial', 'videoIframe', 'aboutUsPageSlug', 'videoPageSlug', 'merchPageSlug', 'newvideo', 'products'));   
-        // return view('layouts.homepage-template', $data);
+        $data['content'] = view('web.homepage_demo', compact('donationGoal', 'videos', 'goalPercent', 'resultsocial', 'videoIframe', 'aboutUsPageSlug', 'videoPageSlug', 'merchPageSlug', 'newvideo', 'products'));   
+        return view('layouts.homepage-template', $data);
     }
 
     public function subscribe(Request $request) {
