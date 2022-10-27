43319d4a98c4b2a6618d71a1ec0d4664a0ecf400
iamkenken <kenneth.froyalde23@gmail.com>
2022-10-27 13:30:42 +0800

    adjust font size

--- resources/views/layouts/web-template.php
+++ resources/views/layouts/web-template.php
@@ -47,7 +47,7 @@
         <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,600i,700" rel="stylesheet">  -->
 
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link href="<?= asset('css/style.css?v=4.15') ?>" type="text/css" rel="stylesheet" />
+        <link href="<?= asset('css/style.css?v=4.16') ?>" type="text/css" rel="stylesheet" />
         <link href="<?= asset('css/flexslider.css') ?>" type="text/css" rel="stylesheet" />
         <!-- <link href="{{ asset('css/jquery.bxslider.css') }}" type="text/css" rel="stylesheet" /> -->
         <link href="<?= asset('css/select2.css') ?>" type="text/css" rel="stylesheet" />
