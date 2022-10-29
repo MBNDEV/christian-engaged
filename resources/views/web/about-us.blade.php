da95f46aafbefd2332b1339cc27372b655bb073e
iamkenken <kenneth.froyalde23@gmail.com>
2022-10-29 10:31:54 +0800

    update image

--- resources/views/web/about-us.blade.php
+++ resources/views/web/about-us.blade.php
@@ -364,7 +364,7 @@
             <?php foreach ($resultsocial as $video1) { ?>
             <a class="animate-fade  bounce-in delay-200 po-bounce-in photoGrid clearfix" id="play" target="blank" href="{{url($video1->video_url)}}">
                     <!-- <img src="{{asset('/uploads/videoimages/'.$video1->video_image) }} " class="img-responsive" width="100%" height="100%" alt="Video" onerror="this.src='{{asset("/images/no_image.png") }}'"> -->
-                    <img src="{{asset('/images/donate-bg-3.png') }} " class="img-responsive" width="100%" height="100%" alt="Video" onerror="this.src='{{asset("/images/no_image.png") }}'">
+                    <img src="{{asset('/images/donate-bg-4.png') }} " class="img-responsive" width="100%" height="100%" alt="Video" onerror="this.src='{{asset("/images/no_image.png") }}'">
                 </a>
              <?php } ?>   
         </figure>
